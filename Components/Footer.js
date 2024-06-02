8b587e9fd70fd7f14c682f5dc1af3a9b82296151
MarChurra <126945275+MarChurra@users.noreply.github.com>
2024-06-02 19:40:15 +0100

    updates

--- Components/Footer.js
+++ Components/Footer.js
@@ -3,9 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer>
-            <img className="icon--unavailable" src="./Images/twitter.png" />
-            <img className="icon--unavailable" src="./Images/facebook.png" />
-            <img className="icon--unavailable" src="./Images/instagram.png" />
+            <a href="https://www.instagram.com/marco_churra/"><img className="icon--unavailable" src="./Images/instagram.png" /> </a>
             <a href="https://linkedin.com/in/marcochurra"> <img src="./Images/linkedin.png" /> </a>
             <a href="https://github.com/MarChurra"> <img src="./Images/github.png" /> </a>
         </footer>
